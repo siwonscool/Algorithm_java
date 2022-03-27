2a62ddfc36f21302b42d834d3e032d3c38922d5f
김쿨 <67731034+siwonscool@users.noreply.github.com>
2022-03-27 15:38:13 +0900

    2022/03/27 알고리즘 스터디 피드백

--- src/leetCode_Queue_medium/no362/HitCounter.java
+++ src/leetCode_Queue_medium/no362/HitCounter.java
@@ -23,6 +23,37 @@ class HitCounter {
 
         return queue.size();
     }
+
+//    int[] time = new int[300];
+//    int[] count = new int[300];
+//
+//    public HitCounter() {
+//
+//    }
+//
+//    //O(1)
+//    public void hit(int timestamp) {
+//        int idx = timestamp % 300;
+//        if(time[idx] != timestamp){
+//            time[idx] = timestamp;
+//            count[idx]=1;
+//        }else{
+//            count[idx]++;
+//        }
+//    }
+//
+//    //O(N)
+//    public int getHits(int timestamp) {
+//        int cnt = 0;
+//
+//        for(int i =0; i < 300;i++){
+//            if(timestamp - time[i] < 300){
+//                cnt+=count[i];
+//            }
+//        }
+//
+//        return cnt;
+//    }
 }
 
 /**
