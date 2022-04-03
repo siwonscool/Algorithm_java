954864721f2709c024189d1e34a92ddf786aa541
김쿨 <67731034+siwonscool@users.noreply.github.com>
2022-04-03 22:03:15 +0900

    2022/04/02 BFS/DFS Graph 수정

--- src/leetCode_BFS_DFS_easy/no1971/Solution.java
+++ src/leetCode_BFS_DFS_easy/no1971/Solution.java
@@ -6,6 +6,8 @@ import java.util.Queue;
 import java.util.Set;
 
 class Solution {
+    //그래프 문제를 풀때는 그래프 하나를 만들어놓고 반복문 도는게 좋다. input 을 전부 돌필요는 없음!
+    //O(ne)
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Queue<Integer> queue = new LinkedList<>();
         Set<Integer> set = new HashSet<>();
@@ -23,16 +25,16 @@ class Solution {
             for (int i = 0; i < edges.length; i++) {
                 // 0열에서 1열로 연결되는 상황
                 if(edges[i][0] == currentNode && !set.contains(edges[i][1])){
-                    set.add(edges[i][0]);
-                    queue.add(edges[i][0]);
+                    set.add(edges[i][1]);
+                    queue.add(edges[i][1]);
                     if(destination == edges[i][1]){
                         return true;
                     }
                 }
                 // 1열에서 0열로 연결되는 상황
                 else if (edges[i][1] == currentNode && !set.contains(edges[i][0])){
-                    set.add(edges[i][1]);
-                    queue.add(edges[i][1]);
+                    set.add(edges[i][0]);
+                    queue.add(edges[i][0]);
                     if (destination == edges[i][0]){
                         return true;
                     }
@@ -42,4 +44,9 @@ class Solution {
 
         return false;
     }
+
+    //disjoint set union
+    //n = 5
+    //int[] parent = {0,1,2,3,4}
+    //모든 node 들이 하나의 연결된 그룹안에 있느냐를 판단해서 source destination 을 판단
 }
