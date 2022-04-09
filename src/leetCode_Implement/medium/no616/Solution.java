590a47b5d3e6cd425f9844e06297067a369dab5e
siwonscool <kimsea13@naver.com>
2022-04-09 22:05:02 +0900

    String 구현문제

--- src/leetCode_Implement/medium/no616/Solution.java
+++ src/leetCode_Implement/medium/no616/Solution.java
@@ -5,10 +5,42 @@ import java.util.*;
 public class Solution {
 
     public String addBoldTag(String s, String[] words) {
-        boolean[]
+        Deque<Integer> queue = new LinkedList<>();
+
+        int flag = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (String word : words){
+                if (s.startsWith(word,i)){
+                    if (queue.isEmpty()){
+                        queue.add(i);
+                    }else if (queue.peekLast() > i){
+                        queue.pollFirst();
+                    }else if (queue.peekLast() != i){
+                        queue.add(i);
+                    }
+
+                    flag = Math.max(flag,i + word.length());
+                }
+            }
+
+            if (queue.peekLast() != flag){
+                queue.add(flag);
+            }
+        }
+
+        queue.stream().forEach(System.out::println);
+
         StringBuffer result = new StringBuffer(s);
 
-        return result+"";
+        int updateLength = 0;
+        while(!queue.isEmpty()){
+            result.insert(queue.pollFirst()+updateLength, "<b>");
+            updateLength += 3;
+            result.insert(queue.pollFirst()+updateLength, "</b>");
+            updateLength += 4;
+        }
+
+        return result.toString();
 
     }
 }
