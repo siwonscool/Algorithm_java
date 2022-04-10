7ea2d08fae80e8d092f6c17dd3ef93ed9ad459ab
김쿨 <67731034+siwonscool@users.noreply.github.com>
2022-04-10 16:26:41 +0900

    2022/04/10 String Implement 스터디

--- src/leetCode_Implement/medium/no616/Solution.java
+++ src/leetCode_Implement/medium/no616/Solution.java
@@ -4,8 +4,12 @@ import java.util.*;
 
 public class Solution {
 
+    //문자열 s를 words 로 마킹 !!
+    //내가직접 마킹을 해본다고 생각해보자 !!
+    //XML parser
+    //내가 기존에 썻던것은 merge interval
     public String addBoldTag(String s, String[] words) {
-        boolean[]
+
         StringBuffer result = new StringBuffer(s);
 
         return result+"";
